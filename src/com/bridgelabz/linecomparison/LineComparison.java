dd63ac50cb437a1225d7f180eb35f45a2f25003c
Rohit <rohitkamble10792@gmail.com>
2022-05-28 17:30:48 +0530

    Calculated Length

--- src/com/bridgelabz/linecomparison/LineComparison.java
+++ src/com/bridgelabz/linecomparison/LineComparison.java
@@ -2,7 +2,19 @@ package com.bridgelabz.linecomparison;
 
 public class LineComparison {
     public static void main(String[] args) {
-        System.out.println("Welcome to Line Comparison program");
 
+        // point 1
+        double x1 = 1;
+        double y1 = 1;
+
+        // point 2
+        double x2 = 5;
+        double y2 = 5;
+
+        // Calculate distance between two points
+        double distance = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+
+        System.out.println("Distance between two points :" + distance);
     }
 }
+
